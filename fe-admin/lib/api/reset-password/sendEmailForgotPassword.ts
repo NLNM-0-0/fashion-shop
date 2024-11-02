86f1ce38f32a64beaffd1d4e8fb68d65cbb84001
kimthu09 <21521495@gm.uit.edu.vn>
2024-11-02 18:53:20 +0700

    feat: authentication

---
+++ fe-admin/lib/api/reset-password/sendEmailForgotPassword.ts
@@ -0,0 +1,24 @@
+import axios from "../axios";
+
+export default async function sendEmailForgotPassword({
+  email,
+}: {
+  email: string;
+}) {
+  const url = `/admin/auth/reset_password`;
+  const data = {
+    email: email,
+  };
+
+  const res = axios
+    .post(url, data)
+    .then((response) => {
+      console.log(response);
+      if (response) return response.data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      return error.response.data;
+    });
+  return res;
+}
