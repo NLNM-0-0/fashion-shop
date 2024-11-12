dd51860d01a7b19995a67c9c057e02aab674bf1d
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-11-12 13:21:38 +0700

    fix: make Java code run normally

--- backend/src/main/java/com/fashion/backend/payload/item/ItemSpecs.java
+++ backend/src/main/java/com/fashion/backend/payload/item/ItemSpecs.java
@@ -3,17 +3,27 @@ package com.fashion.backend.payload.item;
 import com.fashion.backend.entity.Item;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.List;
+
 public class ItemSpecs {
 	public static Specification<Item> hasName(String name) {
 		return (root, query, cb) -> cb.like(root.get("name"), "%" + name + "%");
 	}
 
-	public static Specification<Item> hasGender(String gender) {
-		return (root, query, cb) -> cb.equal(root.get("gender"), gender);
+	public static Specification<Item> hasGender(List<String> genders) {
+		return (root, query, cb) -> root.get("gender").in(genders);
 	}
 
-	public static Specification<Item> hasSeason(String season) {
-		return (root, query, cb) -> cb.equal(root.get("season"), season);
+	public static Specification<Item> hasSeason(List<String> seasons) {
+		return (root, query, cb) -> root.get("season").in(seasons);
+	}
+
+	public static Specification<Item> hasColor(List<String> colors) {
+		return (root, query, cb) -> root.get("color").in(colors);
+	}
+
+	public static Specification<Item> hasCategoryId(Long categoryId) {
+		return (root, query, cb) -> cb.equal(root.get("categories").get("id"), categoryId);
 	}
 
 	public static Specification<Item> hasCategoryName(String categoryName) {
