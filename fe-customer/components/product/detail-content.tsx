aba20ca9dd858d21490f944a74505bf22a6c3953
kimthu09 <21521495@gm.uit.edu.vn>
2024-11-21 16:27:31 +0700

    fix: product detail image and skeleton

--- fe-customer/components/product/detail-content.tsx
+++ fe-customer/components/product/detail-content.tsx
@@ -3,13 +3,19 @@
 import ProductImage from "./product-image";
 import ProductContentForm from "./product-content-form";
 import { useProduct } from "@/hooks/useProduct";
+import ProductDetailSkeleton from "./product-detail-skeleton";
 
 const DetailContent = ({ id }: { id: string }) => {
   const { data, isLoading, error } = useProduct({ id: id });
   const product = data?.data;
 
   if (error) return <>Failed to load.</>;
-  else if (isLoading || !data || !product) return <>Skeleton...</>;
+  else if (isLoading || !data || !product)
+    return (
+      <>
+        <ProductDetailSkeleton />
+      </>
+    );
   else
     return (
       <div className="flex xl:flex-row flex-col gap-9 w-full justify-center items-center">
